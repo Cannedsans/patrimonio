fd0051a8d48054dbc3ba0886a5020cf920ccb607
João Felipe <joaofilipera13@gmail.com>
2025-02-17 20:11:39 -0300

    adm

--- objetos/admin.py
+++ objetos/admin.py
@@ -1,8 +1,43 @@
 from django.contrib import admin
-from .models import Categoria, Fornecedor, Departamento, Movimentacao,Manutencao
-# Register your models here.
+from .models import Categoria, Fornecedor, Departamento, Movimentacao, Manutencao
+
+@admin.register(Manutencao)
+class ManutencaoAdmin(admin.ModelAdmin):
+    list_display = ('bem', 'data_agendada', 'descricao', 'status')
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return ['bem', 'data_agendada', 'descricao']
+        return []
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return True
+
+    def get_fields(self, request, obj=None):
+        if obj:
+            return ['status']
+        return ['bem', 'data_agendada', 'descricao', 'status']
+
+@admin.register(Movimentacao)
+class MovimentacaoAdmin(admin.ModelAdmin):
+    list_display = ('bem', 'de_departamento', 'para_departamento', 'data', 'responsavel')
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return True
+
+# Registrar os modelos sem personalização
 admin.site.register(Categoria)
 admin.site.register(Departamento)
 admin.site.register(Fornecedor)
-admin.site.register(Movimentacao)
-admin.site.register(Manutencao)
